6efd38936dce5fc75c595fd7230c4844d463c1a2
Natval <devennatval@gmail.com>
2021-06-16 01:34:53 +0700

    Update Movement (1-18 done)

--- Oktana/Movement Model/Movement.swift
+++ Oktana/Movement Model/Movement.swift
@@ -106,7 +106,7 @@ struct Movements {
                                     InstructionsGenerate(instructions: "1. Stand in fight stance"),
                                     InstructionsGenerate(instructions: "2. Bend left arm at 90-degree angle and swing, pivot rear heel when throwing punch"),
                                     InstructionsGenerate(instructions: "3. Snap left arm back to face again"),
-                                    InstructionsGenerate(instructions: "4. Rotate right hip forward, pivot on ball of right foot, loop and swing right."),
+                                    InstructionsGenerate(instructions: "4. Rotate right hip forward, pivot on ball of right foot, loop and swing right hand up."),
                                     InstructionsGenerate(instructions: "5. Back to fight stance again"),
       
                                 ],
@@ -197,7 +197,7 @@ struct Movements {
                                 costEPGenerate: 180),
                 MovementGenerate(
                                 movementIDGenerate: 8,
-                                category: 4,
+                                category: 2,
                                 iconMovementGenerate: "",
                                 animationMovementGenerate: [
                                     AnimationGenerate(animationFrame: ""),
@@ -212,7 +212,61 @@ struct Movements {
                                 calorieBurnGenerate: 0,
                                 costEPGenerate: 200),
                 MovementGenerate(
-                                movementIDGenerate: 13,
+                                movementIDGenerate: 9,
+                                category: 1,
+                                iconMovementGenerate: "",
+                                animationMovementGenerate: [
+                                    AnimationGenerate(animationFrame: ""),
+                                    AnimationGenerate(animationFrame: "")
+                                ],
+                                namaMovementGenerate: "Jumping Jack",
+                                instructionsGenerate: [
+                                    InstructionsGenerate(instructions: "1. Stand upright with your legs together, arms at your sides."),
+                                    InstructionsGenerate(instructions: "2. Bend your knees slightly, and jump into the air."),
+                                    InstructionsGenerate(instructions: "3. As you jump, spread your legs to be about shoulder-width apart. Stretch your arms out and over your head."),
+                                    InstructionsGenerate(instructions: "4. Jump back to starting position.")
+                                ],
+                                calorieBurnGenerate: 0,
+                                costEPGenerate: 0),
+                MovementGenerate(
+                                movementIDGenerate: 10,
+                                category: 1,
+                                iconMovementGenerate: "",
+                                animationMovementGenerate: [
+                                    AnimationGenerate(animationFrame: ""),
+                                    AnimationGenerate(animationFrame: "")
+                                ],
+                                namaMovementGenerate: "Mountain Climber",
+                                instructionsGenerate: [
+                                    InstructionsGenerate(instructions: "1. Start from a high plank position with your hands stacked directly under your shoulders."),
+                                    InstructionsGenerate(instructions: "2. Drive one knee forward toward your chest while engaging your abs."),
+                                    InstructionsGenerate(instructions: "3. Return to your plank position, then drive the knee of your opposite foot in. Repeat the movement, alternating legs and speeding up your movements.")
+                                ],
+                                calorieBurnGenerate: 0,
+                                costEPGenerate: 200),
+                MovementGenerate(
+                                movementIDGenerate: 11,
+                                category: 2,
+                                iconMovementGenerate: "",
+                                animationMovementGenerate: [
+                                    AnimationGenerate(animationFrame: ""),
+                                    AnimationGenerate(animationFrame: "")
+                                ],
+                                namaMovementGenerate: "Burpees",
+                                instructionsGenerate: [
+                                    InstructionsGenerate(instructions: "1. Start in a squat position with your knees bent, back straight, and your feet about shoulder-width apart."),
+                                    InstructionsGenerate(instructions: "2. Lower your hands to the floor in front of you so they’re just inside your feet."),
+                                    InstructionsGenerate(instructions: "3. With your weight on your hands, kick your feet back so you’re on your hands and toes, and in a pushup position."),
+                                    InstructionsGenerate(instructions: "4. Keeping your body straight from head to heels, do one pushup. Remember not to let your back sag or to stick your butt in the air."),
+                                    InstructionsGenerate(instructions: "5. Do a frog kick by jumping your feet back to their position."),
+                                    InstructionsGenerate(instructions: "6. Stand and reach your arms over your head."),
+                                    InstructionsGenerate(instructions: "7. Jump quickly into the air so you land back where you started."),
+                                    InstructionsGenerate(instructions: "8. As soon as you land with knees bent, get into a squat position and do another repetition.")
+                                ],
+                                calorieBurnGenerate: 0,
+                                costEPGenerate: 0),
+                MovementGenerate(
+                                movementIDGenerate: 12,
                                 category: 2,
                                 iconMovementGenerate: "",
                                 animationMovementGenerate: [
@@ -227,9 +281,9 @@ struct Movements {
                                     InstructionsGenerate(instructions: "4. Push youself back up.")
                                 ],
                                 calorieBurnGenerate: 0,
-                                costEPGenerate: 200),
+                                costEPGenerate: 0),
                 MovementGenerate(
-                                movementIDGenerate: 14,
+                                movementIDGenerate: 13,
                                 category: 2,
                                 iconMovementGenerate: "",
                                 animationMovementGenerate: [
@@ -243,9 +297,9 @@ struct Movements {
                                     InstructionsGenerate(instructions: "3. Return to standing by walk your hand back.")
                                 ],
                                 calorieBurnGenerate: 0,
-                                costEPGenerate: 200),
+                                costEPGenerate: 0),
                 MovementGenerate(
-                                movementIDGenerate: 15,
+                                movementIDGenerate: 14,
                                 category: 2,
                                 iconMovementGenerate: "",
                                 animationMovementGenerate: [
@@ -255,44 +309,77 @@ struct Movements {
                                 namaMovementGenerate: "Plank Push Up",
                                 instructionsGenerate: [
                                     InstructionsGenerate(instructions: "1. Start in a plank position"),
-                                    InstructionsGenerate(instructions: "2. Place down one hand at a time to lift up into a pushup position."),
-                                    InstructionsGenerate(instructions: "3. Keep back straight and core tight"),
-                                    InstructionsGenerate(instructions: "4. Move one arm at a time back into the plank position.")
+                                    InstructionsGenerate(instructions: "2. Place down one hand at a time to lift up into a push up position."),
+                                    InstructionsGenerate(instructions: "3. Keep back straight and core tight."),
+                                    InstructionsGenerate(instructions: "4. Move one arm at a time back into the plank position."),
 
                                 ],
                                 calorieBurnGenerate: 0,
-                                costEPGenerate: 200),
+                                costEPGenerate: 0),
                 MovementGenerate(
-                                movementIDGenerate: 12,
+                                movementIDGenerate: 15,
                                 category: 2,
                                 iconMovementGenerate: "",
                                 animationMovementGenerate: [
                                     AnimationGenerate(animationFrame: ""),
                                     AnimationGenerate(animationFrame: "")
                                 ],
-                                namaMovementGenerate: "Inchworm",
+                                namaMovementGenerate: "Alternating Hook",
                                 instructionsGenerate: [
-                                    InstructionsGenerate(instructions: ""),
-                                    InstructionsGenerate(instructions: ""),
-                                    InstructionsGenerate(instructions: "")
+                                    InstructionsGenerate(instructions: "1. Stand in fight stance"),
+                                    InstructionsGenerate(instructions: "2. Bend left arm at 90-degree angle and swing, pivot rear heel when throwing punch"),
+                                    InstructionsGenerate(instructions: "3. Snap left arm back to face again"),
+                                    InstructionsGenerate(instructions: "4. Do it with your other arm and repeat")
                                 ],
                                 calorieBurnGenerate: 0,
-                                costEPGenerate: 200),
+                                costEPGenerate: 0),
                 MovementGenerate(
-                                movementIDGenerate: 10,
+                                movementIDGenerate: 16,
                                 category: 2,
                                 iconMovementGenerate: "",
                                 animationMovementGenerate: [
                                     AnimationGenerate(animationFrame: ""),
                                     AnimationGenerate(animationFrame: "")
                                 ],
-                                namaMovementGenerate: "Inchworm",
+                                namaMovementGenerate: "Alternating Uppercut",
                                 instructionsGenerate: [
-                                    InstructionsGenerate(instructions: ""),
-                                    InstructionsGenerate(instructions: ""),
-                                    InstructionsGenerate(instructions: "")
+                                    InstructionsGenerate(instructions: "1. Stand in fight stance"),
+                                    InstructionsGenerate(instructions: "4. Rotate right hip forward, pivot on ball of right foot, loop and swing right hand up."),
+                                    InstructionsGenerate(instructions: "5. Back to fight stance again and do it with your other arm."),
                                 ],
                                 calorieBurnGenerate: 0,
-                                costEPGenerate: 200),
+                                costEPGenerate: 0),
+                MovementGenerate(
+                                movementIDGenerate: 17,
+                                category: 2,
+                                iconMovementGenerate: "",
+                                animationMovementGenerate: [
+                                    AnimationGenerate(animationFrame: ""),
+                                    AnimationGenerate(animationFrame: "")
+                                ],
+                                namaMovementGenerate: "Jab Cross Hook Uppercut",
+                                instructionsGenerate: [
+                                    InstructionsGenerate(instructions: "1. Stand in fight stance"),
+                                    InstructionsGenerate(instructions: "2. Do a jab followed by a cross, hook and then uppercut"),
+                                ],
+                                calorieBurnGenerate: 0,
+                                costEPGenerate: 0),
+                MovementGenerate(
+                                movementIDGenerate: 18,
+                                category: 3,
+                                iconMovementGenerate: "",
+                                animationMovementGenerate: [
+                                    AnimationGenerate(animationFrame: ""),
+                                    AnimationGenerate(animationFrame: "")
+                                ],
+                                namaMovementGenerate: "Sit Up Jab Straight",
+                                instructionsGenerate: [
+                                    InstructionsGenerate(instructions: "1. Lie down on your back and bend your legs to stabilize your lower body."),
+                                    InstructionsGenerate(instructions: "2. Cross your hands to opposite shoulders or place them behind your ears, without pullling on your neck."),
+                                    InstructionsGenerate(instructions: "3. Curl your upper body all the way up toward your knees. Exhale as you lift."),
+                                    InstructionsGenerate(instructions: "4. Do a jab and cross, slowly lower yourself down, returning to your starting point. Inhale as you lower.")
+                                ],
+                                calorieBurnGenerate: 0,
+                                costEPGenerate: 0)
             ]
 }
